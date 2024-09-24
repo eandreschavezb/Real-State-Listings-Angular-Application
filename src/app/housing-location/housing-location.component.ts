bb1b8f4a84ec0f7da489549a0edb58f4a0c8eb5c
eandreschavezb <andreschavezbermeo@gmail.com>
2024-09-24 16:05:09 +0800

    Styling changes

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -10,9 +10,11 @@ import { RouterModule } from '@angular/router';
   template: `
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{ housingLocation.name }}">
+  <div class="description-container">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
     <a [routerLink]="['/details',housingLocation.id]">MAS DETALLES</a>
+  </div>
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
