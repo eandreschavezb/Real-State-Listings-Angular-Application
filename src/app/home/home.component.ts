5a9513f17b6b0022b1fc1ea554bd09afc4ffdba1
eandreschavezb <andreschavezbermeo@gmail.com>
2025-02-09 18:47:45 -0500

    search function working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, FormsModule, HousingLocationComponent],
   template: `
 
   <section class="hero-bg">
@@ -19,15 +20,15 @@ import { HousingService } from '../housing.service';
     <div class="input-containers">
       <div class="input-card">
         <form>
-          <input type="text" placeholder="Buscar propiedades de renta">
-          <button class="primary" type="button">BUSCAR</button>
+          <input type="text" [(ngModel)]="searchQuery" [ngModelOptions]="{standalone: true}" placeholder="Buscar propiedades de renta">
+          <button class="primary" (click)="filterLocations()" type="button">BUSCAR</button>
         </form>
       </div>
     </div>
   </section>
   <section class="results">
     <app-housing-location 
-      *ngFor="let housingLocation of housingLocationList" 
+      *ngFor="let housingLocation of filteredHousingLocationList" 
       [housingLocation]="housingLocation">
     </app-housing-location>
   </section>
@@ -36,11 +37,32 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent {
   housingLocationList:HousingLocation[] = [];
+
+  filteredHousingLocationList: HousingLocation[] = []; // array con la lista filtrada de anuncios
+  searchQuery: string = ''; // query para filtrar anuncios
+
   housingService: HousingService = inject (HousingService);
 
   constructor () {
-this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+  this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
   this.housingLocationList = housingLocationList;
+
+  this.filteredHousingLocationList = housingLocationList; // muestra todas las locaciones cuando no hay query
+
+
 });
   }
+
+
+  filterLocations() {
+
+
+    const query = this.searchQuery.toLowerCase().trim(); //convierte en minusculas
+    this.filteredHousingLocationList = this.housingLocationList.filter(location =>
+      location.city.toLowerCase().includes(query) // toma el query y filtra el array
+    );
+    
+    console.log(this.filteredHousingLocationList);
+  }
+
 }
