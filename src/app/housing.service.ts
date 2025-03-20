0867126be4a0fd71324a037d6d803bbd1cdf2003
eandreschavezb <127151947+eandreschavezb@users.noreply.github.com>
2025-03-20 05:16:45 -0500

    Implementing accordions
    Managed to add accordion package by manually installing accordion component and avoiding issue updating tailwind config file due to custom fonts conflict.
    Customized component styling and filled with content.

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -6,7 +6,7 @@ import { HousingLocation } from './housing-location';
 })
 export class HousingService {
  
-  url = 'http://localhost:3000/locations';
+  url = 'http://localhost:5000/locations';
   
     constructor() {}
 
